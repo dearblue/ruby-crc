d59372360212f0a6493d3e3872e54330037acba5
dearblue <dearblue@users.noreply.github.com>
2017-03-28 23:17:44 +0900

    acrc のためのテストコードを追加

---
+++ test/test_arc.rb
@@ -0,0 +1,23 @@
+#!ruby
+
+require "test-unit"
+require_relative "common"
+require "crc"
+require "crc/acrc"
+require "securerandom"
+
+class TestArcCRC < Test::Unit::TestCase
+  $testmodules.each do |crc|
+    class_eval(<<-"EOS", __FILE__, __LINE__ + 1)
+      def test_arc_#{crc.to_s.slice(/\w+$/)}
+        crc = #{crc}
+        [["", "", 12345],
+         ["123456789", nil, 1234567],
+         ["", "123456789", 123456789]].each do |a, b, t|
+          t &= crc.bitmask
+          assert_equal(t, crc.crc(a + crc.acrc(a, b, t) + (b || "")))
+        end
+      end
+    EOS
+  end
+end
